5d86b12875518b2281130125713462f679fc63a3
mafeifan <maf@shinetechchina.com>
2019-01-01 20:58:21 +0800

    add pxc mysql cluster demo

---
+++ percona-xtradb-cluster/readme.md
@@ -0,0 +1,3 @@
+docker学习系列13 实现基于 pxc 的 mysql 多节点主主同步
+
+https://www.jianshu.com/p/eae6b12c1cd5
---
+++ percona-xtradb-cluster/run.sh
@@ -0,0 +1,14 @@
+# 拉镜像
+docker pull percona/percona-xtradb-cluster:5.7
+
+# 给镜像设置别名
+docker tag percona/percona-xtradb-cluster:5.7 pxc
+
+# 创建网络并设置网段和子网掩码
+docker network create --subnet=172.19.0.0/24 pxc-network
+
+# 创建三个mysql节点
+
+docker run -d -p 33010:3306 -e MYSQL_ROOT_PASSWORD=root -e CLUSTER_NAME=pxc_cluster --name=pxc_node1 --net=pxc-network --ip=172.19.0.2 pxc
+docker run -d -p 33011:3306 -e MYSQL_ROOT_PASSWORD=root -e CLUSTER_NAME=pxc_cluster -e CLUSTER_JOIN=pxc_node1 --name=pxc_node2 --net=pxc-network --ip=172.19.0.3 pxc
+docker run -d -p 33012:3306 -e MYSQL_ROOT_PASSWORD=root -e CLUSTER_NAME=pxc_cluster -e CLUSTER_JOIN=pxc_node1 --name=pxc_node3 --net=pxc-network --ip=172.19.0.4 pxc
